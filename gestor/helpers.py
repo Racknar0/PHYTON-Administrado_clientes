03c048e214a10b44789e51f32c2ede59e78d85d7
Camilo Torres <racknarow1@gmail.com>
2022-06-23 20:09:24 -0500

    Add clear Console Multplattform

--- gestor/helpers.py
+++ gestor/helpers.py
@@ -1,1 +1,10 @@
-""" funciones de ayuda """
+""" Funciones de ayuda """
+
+import os
+import platform
+
+def clear():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
